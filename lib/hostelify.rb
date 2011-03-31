2dcad0ba2360213cb0e3780b17f40f36d0d55b02
Holden Thomas <holden.thomas@gmail.com>
2011-03-31 13:41:00 +0200

    directions added to gem

--- lib/hostelify.rb
+++ lib/hostelify.rb
@@ -5,14 +5,16 @@ require 'nibbler'
 require 'hostelify/hostelworld'
 
 module Hostelify
-  # Your code goes here...
   
   def self.find(*args, &block)
-    options = { :currency => 'EUR', :date_from => '2011-04-06'.to_s }
+    options = { :currency => 'EUR', :date_from => (Date.today+14).to_s, :directions => false }
     options.merge!(args.pop) if args.last.kind_of? Hash
     
-    RestClient.head 'http://www.hostelworld.com/hosteldetails.php/20763' do |response, request, result|
-      @resource = RestClient.post response.headers[:location], 
+    RestClient.head 'http://www.hostelworld.com/hosteldetails.php/' + options[:id].to_s do |response, request, result|
+      redirect = response.headers[:location]
+      redirect = redirect + '/directions' if options[:directions]
+      
+      @resource = RestClient.post redirect, 
         :date_from => Date.strptime(options[:date_from])+1, 
         :date_to => Date.strptime(options[:date_from])+8,
         :searchperformedflag => 1, 

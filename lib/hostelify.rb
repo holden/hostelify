2dbf05c17475701fe14263e8958ea6b9ce50da82
Holden Thomas <holden.thomas@gmail.com>
2011-04-01 13:02:28 +0200

    robottxt friendly

--- lib/hostelify.rb
+++ lib/hostelify.rb
@@ -8,10 +8,16 @@ require 'hostelify/hostelworld'
 module Hostelify
   
   def self.find(*args, &block)
-    options = { :currency => 'EUR', :date_from => Date.today+14, :directions => false }
+    options = { :currency => 'EUR', :date_from => Date.today+14, :directions => false, :robotstxt_check => false }
     options.merge!(args.pop) if args.last.kind_of? Hash
     
-    if Robotstxt.allowed?('http://www.hostelworld.com', 'rowbot')
+    if options[:robotstxt_check]
+      robot_friendly = Robotstxt.allowed?('http://www.hostelworld.com', 'rowbot')
+    else
+      robot_friendly = true
+    end
+    
+    if robot_friendly
       RestClient.head 'http://www.hostelworld.com/hosteldetails.php/' + options[:id].to_s do |response, request, result|
         redirect = response.headers[:location]
         redirect = redirect + '/directions' if options[:directions]
